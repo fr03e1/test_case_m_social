a28b597f6099f98b2402c30f85275fd22d59a32a
fr03e1 <aleksejbocarov65@gmail.com>
2023-06-07 23:18:25 +0300

    add auth controller

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+
+    }
+}

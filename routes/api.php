df00cb8cdff0f3e1c4d26002bf5a39b299a7fcc5
fr03e1 <aleksejbocarov65@gmail.com>
2023-06-08 23:42:01 +0300

    add exception handler

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
+
     Route::controller(AuthController::class)->group(function () {
         Route::post('/auth/register', 'register');
     });
